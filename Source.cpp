5d7b28eeb1f8bcb325447b00e63bef2770d31d52
Violetta-Kiseleva <74498520+Violetta-Kiseleva@users.noreply.github.com>
2020-12-07 02:06:22 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,11 +1,17 @@
-#include <iostream>
+#include <iostream> 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 // GLEW
 #define GLEW_STATIC
 #include <GLFW/glew.h>
 
 // GLFW
-#include <GLFW/glfw3.h>
+#include <GLFW/glfw3.h> 
+
+// SOIL
+#include <SOIL/SOIL.h>
 
 // SHADER
 #include "Shader.h"
@@ -61,63 +67,156 @@ int main()
     Shader file_Shader("vertex_shader.vs", "fragment_shader.vs");
 
 
-    // Устновка вершин (буфер) и указателей
+    // Установка вершин (буфер) и указателей
+    /* ТРЕУГОЛЬНИК
     GLfloat vertices[] = {
             // Позиция           // Цвета
         0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,      // правый низ
        -0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,      // левый низ
         0.0f,  0.5f, 0.0f,   0.0f, 0.0f, 1.0f       // Вершина 
     };
-    GLuint VBO, VAO;
+    */
+
+    // Устновка вершин (буфер) и указателей
+    GLfloat vertices[] = {
+        // Позиции              // Цвета           // Текстуры
+         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // правый верх
+         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // правый низ
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // левый низ
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // левый верх 
+    };
+    GLuint indices[] = {  // Note that we start from 0!
+        0, 1, 3, // первый треугольник
+        1, 2, 3  // второй треугольник
+    };
+
+    GLuint VBO, VAO; 
+    GLuint EBO;                     // прямоугольник
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    //glGenBuffers(1, &EBO);
+    glGenBuffers(1, &EBO);          // прямоугольник
 
-    // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+    // связываем объект массива и устанавливаем буфера и аттрибуты вершин
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); 
 
-    // Position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+    // для прямоугольника 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // Позиция атрибута
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
-    // Color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-    glEnableVertexAttribArray(1);
 
-    glBindVertexArray(0); // Unbind VAO
+    // Цвет атрибута
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1); 
+
+    // Текстура
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(2);
+
+    glBindVertexArray(0);                           // отвязываем VAO 
+
+    // загружаем и создаем текстуру
+    GLuint texture1; 
+    GLuint texture2;
+    //// текстура первая
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);          // все 2д операции влияют на этот объект
+    
+    // устанавливает текстуру
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    
+    // текстура фильтрации параметров
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // загружаем картинку, создаем текстуру
+    int width, height;
+    unsigned char* image = SOIL_load_image("container.jpg", &width, &height, 0, SOIL_LOAD_RGB);
+    
+    if (!image)
+    {
+        std::cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
+    }
+    // генерирую данные текстуры
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);        // привязка к текстуре изображения
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    // освобождение участка памяти 
+    SOIL_free_image_data(image);
+    glBindTexture(GL_TEXTURE_2D, 0);                // отвязка объекта текстуры
+
+    // вторая текстура 
+    // ===================
+    
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    // Set our texture parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // Set texture filtering
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // Load, create texture and generate mipmaps
+    image = SOIL_load_image("awesomeface.png", &width, &height, 0, SOIL_LOAD_RGB); 
+
+    if (!image)
+    {
+        std::cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
+    }
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    SOIL_free_image_data(image);
+    glBindTexture(GL_TEXTURE_2D, 0);
 
 
     ////////////////////////////////////////////////////////        ИГРОВОЙ ЦИКЛ    //////////////////////////////////////////////
     while (!glfwWindowShouldClose(window))
     {
-        // Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
+        // проверка на события (мышь, нажатие клавиши) 
         glfwPollEvents();
 
-        // Render
-        // Clear the colorbuffer
+        // очищаем буфер цвета
         glClearColor(0.9f, 0.3f, 0.2f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Draw the triangle
+        // привязка текстуры
+        // glBindTexture(GL_TEXTURE_2D, texture); 
+
+        // рисуем прямоугольник
         file_Shader.Use();
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glUniform1i(glGetUniformLocation(file_Shader.Program, "ourTexture1"), 0);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+        glUniform1i(glGetUniformLocation(file_Shader.Program, "ourTexture2"), 1);
+
+
+        glBindVertexArray(VAO);  
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+      //  glDrawArrays(GL_TRIANGLES, 0, 3);   // треугольник
         glBindVertexArray(0);
 
-        // Swap the screen buffers
+        // Меняем местами буфера экрана
         glfwSwapBuffers(window);
     }
-    // Properly de-allocate all resources once they've outlived their purpose
+    // удаляем все ресурсы после завершения их работы
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-    // Terminate GLFW, clearing any resources allocated by GLFW.
+    glDeleteBuffers(1, &EBO);
+    // окончание работы GLFW, очищаем выделенные ресурсы
     glfwTerminate();
     return 0;
 }
 
-// Is called whenever a key is pressed/released via GLFW
+// вызывается, когда нажата отпущена клавиша GLFW
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
